aedb9d49f68d2f6fa4a83667d2496148ebd18043
Gleb Yudanov <glebyudanov1@yandex.ru>
2024-12-06 22:39:38 +0500

    Перенос на Vue+vite и реализация журнала пресетов

---
+++ src/components/WorkInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="work">
+    <h4 class="work__title">Работа {{ index + 1 }}</h4>
+    <label>Вес: <input v-model.number="work.weight" type="number" step="0.01" required></label>
+    <label>Максимум: <input v-model.number="work.maxPoints" type="number" step="0.01" required></label>
+    <label>Получено: <input v-model.number="work.achievedPoints" type="number" step="0.01" required></label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    index: Number,
+    modelValue: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    work: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value);
+      }
+    }
+  }
+};
+</script>
